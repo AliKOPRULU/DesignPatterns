f3a79760b7e0ef08e428e16c66f3fbaf4ff0249e
Ali <Ali@DESKTOP-AK>
2017-01-04 15:01:31 +0300

    Abstract Factory Design Pattern

---
+++ DesignPatterns/CreationalPatterns/AbstractFactoryAnimal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPatterns
+{
+    class AbstractFactoryAnimal
+    {
+        class MainApp
+        {            
+            public static void Main()
+            {
+                
+            }
+        }
+    }
+
+
+
+
+
+
+}
