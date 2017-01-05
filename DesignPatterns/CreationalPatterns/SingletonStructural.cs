7f152c64e07d248f10ff285abc4e06c7d174d3e5
Ali <Ali@DESKTOP-AK>
2017-01-05 12:17:11 +0300

    Singleton initail

---
+++ DesignPatterns/CreationalPatterns/SingletonStructural.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPatterns
+{
+    class SingletonStructural
+    {
+        static void Main(string[] args)
+        {
+            // Constructor is protected -- cannot use new
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("NEsneler Aynı");
+            }
+            Console.ReadKey();
+
+        }
+    }
+
+    class Singleton
+    {
+        private static Singleton _instance;
+
+        protected Singleton()
+        {
+
+        }
+
+        public static Singleton Instance()
+        {// Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
