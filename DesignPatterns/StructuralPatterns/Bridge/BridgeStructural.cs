40141e55eefbfab2c391ea038824aa5917fc9c10
Ali <Ali@DESKTOP-AK>
2017-01-07 11:36:34 +0300

    BridgeStructural

---
+++ DesignPatterns/StructuralPatterns/Bridge/BridgeStructural.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StructuralPatterns.Bridge
+{
+    class BridgeStructural
+    {
+        public static void Main(string[] args)
+        {
+            Abstraction ab = new RefinedAbstraction();
+
+            // Set implementation and call
+            ab.Implementor = new ConcreteImplementorA();
+            ab.Operation();
+
+            // Change implemention and call
+            ab.Implementor = new ConcreteImplementorB();
+            ab.Operation();
+
+
+            Console.ReadKey();
+        }
+    }
+
+
+    class Abstraction
+    {
+        protected Implementor implementor;
+
+        // Property
+        public Implementor Implementor
+        {
+            set { implementor = value; }
+        }
+
+        public virtual void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+
+    abstract class Implementor
+    {
+        public abstract void Operation();
+    }
+
+    class RefinedAbstraction : Abstraction
+    {
+        public override void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+
+    class ConcreteImplementorA : Implementor
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteImplementorA Operation");
+        }
+    }
+
+    class ConcreteImplementorB : Implementor
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteImplementorB Operation");
+        }
+    }
+
+
+
+
+}
