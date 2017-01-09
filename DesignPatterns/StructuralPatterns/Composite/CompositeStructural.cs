da9181acb7c71066f35a9c117b61cd89d04546b1
Ali <Ali@DESKTOP-AK>
2017-01-09 11:32:54 +0300

    Composite Sutructural

---
+++ DesignPatterns/StructuralPatterns/Composite/CompositeStructural.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StructuralPatterns.Composite
+{
+    class CompositeStructural
+    {
+        public static void Main(string[] args)
+        {
+            //Create a tree structure
+            Composite root = new Composite("root");
+            root.Add(new Leaf("Leaf A"));
+            root.Add(new Leaf("Leaf B"));
+
+            Composite comp = new Composite("Composite X");
+            comp.Add(new Leaf("Comp XA"));
+            comp.Add(new Leaf("Comp XB"));
+
+            root.Add(comp);
+            root.Add(new Leaf("Leaf C"));
+
+            // Add and remove a leaf
+            Leaf leaf = new Leaf("Leaf D");
+            root.Add(leaf);
+            root.Remove(leaf);
+
+            //Recursive display tree
+            root.Display(1);
+
+            Console.ReadKey();
+        }
+    }
+
+    // The 'Component' abstract class
+    abstract class Component
+    {
+        protected string name;
+
+        // Constructor
+        public Component(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract void Add(Component c);
+        public abstract void Remove(Component c);
+        public abstract void Display(int depth);
+    }
+
+    class Composite : Component
+    {
+        private List<Component> _children = new List<Component>();
+
+        public Composite(string name) : base(name)
+        {
+        }
+
+        public override void Add(Component component)
+        {
+            _children.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            _children.Remove(component);
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+
+            // Recursively display child nodes
+            foreach (Component component in _children)
+            {
+                component.Display(depth + 2);
+            }
+        }
+    }
+
+    // The 'Leaf' class
+    class Leaf : Component
+    {
+        public Leaf(string name) : base(name)
+        {
+        }
+
+        public override void Add(Component c)
+        {
+            Console.WriteLine("Cannot add to a leaf");
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+        }
+
+        public override void Remove(Component c)
+        {
+            Console.WriteLine("Cannot remove from a leaf");
+        }
+    }
+
+
+
+
+
+
+
+}
