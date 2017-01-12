e60e4aa1ba5d6abbb6b636ad798d8c041009ea19
Ali <Ali@DESKTOP-AK>
2017-01-12 15:02:13 +0300

    Memento Structural

---
+++ DesignPatterns/BehavioralPatterns/Memento/MementoStructural.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.BehavioralPatterns.Memento
+{
+    class MementoStructural
+    {
+        public static void Main(string[] args)
+        {
+            Originator o = new Originator();
+            o.State = "On";
+
+            // Store internal state
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
+
+            // Continue changing originator
+            o.State = "Off";
+
+            // Restore saved state
+            o.SetMemento(c.Memento);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Originator//:yaratıcı, fikir babası
+    {
+        private string _state;
+
+        // Property
+        public string State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                Console.WriteLine("State: ", _state);
+            }
+        }
+
+        // Creates memento 
+        public Memento CreateMemento()
+        {
+            return (new Memento(_state));
+        }
+
+        // Restores original state
+        public void SetMemento(Memento memnto)
+        {
+            Console.WriteLine("Restoring state...");
+            State = memnto.State;
+        }
+    }
+
+    // The 'Memento' class 
+    class Memento
+    {
+        private string _state;
+
+        public Memento(string state)
+        {
+            this._state = state;
+        }
+
+        // Gets or sets state
+        public string State
+        {
+            get { return _state; }
+        }
+    }
+
+    // The 'Caretaker' class Caretaker:bakıcı, yönetici
+    class Caretaker
+    {
+        private Memento _memento;
+
+        public Memento Memento
+        {
+            set { _memento = value; }
+            get { return _memento; }
+        }
+    }
+
+
+
+}
