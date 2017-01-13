e17054e606ab4c9bb0bd1216acef102e4eb9cc30
Ali <Ali@DESKTOP-AK>
2017-01-13 09:28:30 +0300

    State Structural

---
+++ DesignPatterns/BehavioralPatterns/State/StateStructural.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.BehavioralPatterns.State
+{
+    class StateStructural
+    {
+        public static void Main(string[] args)
+        {
+            // Setup context in a state
+            Context c = new Context(new ConcreteStateA());
+
+            // Issue requests, which toggles state
+            c.Requets();
+            c.Requets();
+            c.Requets();
+            c.Requets();
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class State
+    {
+        public abstract void Handle(Context context);
+    }
+
+    class ConcreteStateA : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateB();
+
+        }
+    }
+
+    class ConcreteStateB : State
+    {
+        public override void Handle(Context context)
+        {
+            context.State = new ConcreteStateA();
+        }
+    }
+
+
+    class Context
+    {
+        private State _state;
+
+        public Context(State state)
+        {
+            this.State = state;
+        }
+
+        // Gets or sets the state
+        public State State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                Console.WriteLine("State: " + _state.GetType().Name);
+            }
+        }
+
+        public void Requets()
+        {
+            _state.Handle(this);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
