e14b0749cebe4b45fcb19aa92851b9dca0c7925b
Ali <Ali@DESKTOP-AK>
2017-01-13 19:48:28 +0300

    Template Method Db Access (But no Db)

---
+++ DesignPatterns/BehavioralPatterns/TemplateMethod/TemplateMethodDbAccess.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.BehavioralPatterns.TemplateMethod
+{
+    class TemplateMethodDbAccess
+    {
+        public static void Main(string[] args)
+        {
+            DataAccessObject daoCategories = new Categories();
+            daoCategories.Run();
+
+            DataAccessObject daoProducts = new Products();
+            daoProducts.Run();
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+    // The 'AbstractClass' abstract class
+    abstract class DataAccessObject
+    {
+        protected string connectionString;
+        protected DataSet dataSet;
+
+        public virtual void Connect()
+        {
+            // Make sure mdb is available to app
+            connectionString = "provider=Microsoft.JET.OLEDB.4.0; data source=..\\..\\..\\db1.mdb";
+        }
+
+        public abstract void Select();
+        public abstract void Process();
+
+        public virtual void Disconnect()
+        {
+            connectionString = "";
+        }
+
+        // The 'Template Method' 
+        public void Run()
+        {
+            Connect();
+            Select();
+            Process();
+            Disconnect();
+        }
+    }
+
+    class Categories : DataAccessObject
+    {
+        public override void Process()
+        {
+            Console.WriteLine("Categories ---- ");
+
+            DataTable dataTable = dataSet.Tables["Categories"];
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine(row["CategoryName"]);
+            }
+            Console.WriteLine();
+        }
+
+        public override void Select()
+        {
+            string sql = "select CategoryName from Categories";
+            OleDbDataAdapter dataAdaptor = new OleDbDataAdapter(sql, connectionString);
+
+            dataSet = new DataSet();
+            dataAdaptor.Fill(dataSet, "Categories");
+        }
+    }
+
+    class Products : DataAccessObject
+    {
+
+        public override void Select()
+        {
+            string sql = "select ProductName from Products";
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sql, connectionString);
+            dataSet = new DataSet();
+
+            dataAdapter.Fill(dataSet, "Products");
+        }
+        
+        public override void Process()
+        {
+            Console.WriteLine("Products ---- ");
+            DataTable dataTable = dataSet.Tables["Products"];
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine(row["ProductName"]);
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
