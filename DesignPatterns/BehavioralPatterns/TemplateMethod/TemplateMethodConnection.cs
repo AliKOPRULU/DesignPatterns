6f9dc605db432faffa8324d1513b2a23b866acbc
Ali <Ali@DESKTOP-AK>
2017-01-13 17:26:57 +0300

    Template Method Connection Check

---
+++ DesignPatterns/BehavioralPatterns/TemplateMethod/TemplateMethodConnection.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.BehavioralPatterns.TemplateMethod
+{
+    class TemplateMethodConnection
+    {
+        public static void Main(string[] args)
+        {
+            OperationContack o = new OperationContack();
+            o.TemplateInsert();
+            OperationProduct op = new OperationProduct();
+            o.TemplateCheckData();
+
+
+
+            Console.ReadKey();
+        }
+    }
+
+    //Template yapısı
+    abstract class Operation
+    {
+        private void OpenCon()
+        {
+            Console.WriteLine("Connection Open");
+        }
+
+        //bu abstract sınıfı uygulayan sınıfların sadece Insert ve CheckData da yapılacak
+        //işlemleri overwrite etmesi gereklidir.
+        public abstract void Insert();
+        public abstract bool CheckData();
+
+        public void CloseCon()
+        {
+            Console.WriteLine("Connection Close");
+        }
+
+        //Bu sınıfı uygulayan sınıfların her işlemde connection u açmaya ve kapatmaya ihtiyacı kalmamıştır.
+        //connection açma ve kapama işlemleri TemplateMethod larda yapılıyor.
+
+        public void TemplateInsert()
+        {
+            OpenCon();
+            Insert();
+            CloseCon();
+        }
+
+        public bool TemplateCheckData()
+        {
+            OpenCon();
+            bool Check = CheckData();
+            CloseCon();
+            return Check;
+        }
+    }
+
+    //ConcreteTemplateMethod
+    class OperationContack : Operation
+    {
+        public override bool CheckData()
+        {
+            //Kontroller yapıldığını varsayalım...
+            Console.WriteLine("Contact Kayıt Bulundu.");
+            return true;
+        }
+
+        public override void Insert()
+        {
+            Console.WriteLine("Contact Kayıt Eklendi.");
+        }
+    }
+
+    class OperationProduct : Operation
+    {
+        public override void Insert()
+        {
+            Console.WriteLine("Product Kayıt Eklendi.");
+        }
+        public override bool CheckData()
+        {
+            //Kontroller yapıldığını varsayalım...
+            Console.WriteLine("Product Kayıt Bulundu.");
+            return true;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
