2d7948268900c0b909077b99d2f21e8a9bd1ab03
Ali <Ali@DESKTOP-AK>
2017-01-13 17:01:13 +0300

    Template Method Game Reporter

---
+++ DesignPatterns/BehavioralPatterns/TemplateMethod/TemplateMethodGameReporter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.BehavioralPatterns.TemplateMethod
+{
+    class TemplateMethodGameReporter
+    {
+        public static void Main(string[] args)
+        {
+            GameReporter reporter = null;
+
+            reporter = new XmlReporter();
+            reporter.WriteSummary();
+            Console.WriteLine();
+
+            reporter = new TextReporter();
+            reporter.WriteSummary();
+            Console.WriteLine();
+
+
+            reporter = new ConsolerReporter();
+            reporter.WriteSummary();
+
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class GameReporter
+    {
+        public void GetResults()
+        {
+            Console.WriteLine("Oyuncuların istatistikleri toplanıyor");
+        }
+
+        public void ParseResults()
+        {
+            Console.WriteLine("İstatistikler ayrıştırılıyor");
+        }
+
+        public abstract void WriteResults();
+
+        public void WriteSummary()
+        {
+            GetResults();
+            ParseResults();
+            WriteResults();
+        }
+    }
+
+    class XmlReporter : GameReporter
+    {
+        public override void WriteResults()
+        {
+            Console.WriteLine("İstatistikler XML dosyasına yazılıyor.");
+        }
+    }
+
+    class TextReporter : GameReporter
+    {
+        public override void WriteResults()
+        {
+            Console.WriteLine("İstatistikler TEXT dosyasına yazdırılıyor.");
+        }
+    }
+
+    class ConsolerReporter : GameReporter
+    {
+        public override void WriteResults()
+        {
+            Console.WriteLine("İstatistikler CONSOLE ekranına basılıyor.");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
